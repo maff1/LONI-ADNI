a45762c9ac255ba75d10adf02fba2c6ae4554f5b
maff1 <marcoandfernandes@gmail.com>
2021-11-11 12:42:37 +0000

    fix 5

--- CODE/f_009_plot_performance_metrics.R
+++ CODE/f_009_plot_performance_metrics.R
@@ -1,5 +1,4 @@
 ################################################################################
-################################################################################
 # ADNI NIGHTINGALE (NG) LONGITUDINAL ## V001 ## MAFF ###########################
 # F009 DISTRIBUTION PERFORMANCE METRICS:
 #    + BY PHENOTYPE
@@ -8,29 +7,115 @@
 library(ggplot2)
 library(data.table)
 library(tidyverse)
+library(dendsort)
+library(circlize)
+library(ComplexHeatmap)
+library(gridBase)
 
+lsPheno <- list.files(path = "./RESULTS", pattern = "_cv10", full.names = TRUE)
+names(lsPheno) <- gsub("_cv10_lmx.rds", "", basename(lsPheno))
+temp = readRDS(lsPheno[1])[, c(301:307)]
+# ---------------------------------------------------------------------------- #
 
-temp = readRDS("./RESULTS/BPDIA_cv10_lmx.rds")[, c(301:307)]
-temp %>%
+dat1 <- temp %>%
   group_by(metabolites) %>%
-  dplyr::summarize(Mean = mean(RMSE, na.rm=TRUE)) %>%
-  dplyr::arrange(Mean)
+  dplyr::summarize(AIC = mean(AIC, na.rm=TRUE),
+                   R2 = mean(R2, na.rm=TRUE), 
+                   RMSE = mean(RMSE, na.rm=TRUE)
+  )
+datAIC <- data.frame(AIC = dat1[, 2], 
+                     row.names = tolower(
+                       gsub("_", ".", dat1$metabolites)
+                     )
+)
 
+datR2 <- data.frame(R2 = dat1[, 3], 
+                    row.names = tolower(
+                      gsub("_", ".", dat1$metabolites)
+                    )
+)
 
-L_HDL_C <- subset(temp, metabolites == "L_HDL_C")
+datRMSE <- data.frame(RMSE = dat1[, 4], 
+                      row.names = tolower(
+                        gsub("_", ".", dat1$metabolites)
+                      )
+)
+# ---------------------------------------------------------------------------- #
 
-pdf(file = "./RESULTS/plot_performance_metrics_BPDIA_L_HDL_C.pdf")
-L_HDL_C %>%
-  gather(.,MSE:BIC,key ="Metric",value = "Value")%>%
-  ggplot(aes(x=Metric,y=Value,fill=Metric)) +
-  geom_boxplot()+coord_flip() +
-  facet_wrap(~Metric,ncol=1,scales="free") +
-  theme_bw() +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    plot.subtitle = element_text(color = "blue")
-    ) +
-  labs(title = "Pheno::BPDIA",
-       subtitle = "L_HDL_C",
-       caption = "Data source: Nightingale longitudinal")
+split = factor(letters[1:5], levels = letters[1:5])
+col_funAIC = colorRamp2(breaks = c(0, 7500, 15000), 
+                        colors = c("white", "yellow", "red"), transparency = 0.25)
+col_funR2 = colorRamp2(breaks = c(0, 0.5, 1), 
+                       colors = c("white", "yellow", "red"), transparency = 0.25)
+col_funRMSE = colorRamp2(breaks = c(0, 0.5, 1), 
+                         colors = c("white", "yellow", "red"), transparency = 0.25)
+
+lgd_aic = Legend(title = "AIC", col_fun = col_funAIC)
+lgd_r2 = Legend(title = "r2", col_fun = col_funR2)
+lgd_rmse = Legend(title = "RMSE", col_fun = col_funRMSE)
+# ---------------------------------------------------------------------------- #
+
+pdf(file = "./RESULTS/circosMetrics_BPDIA.pdf", width = 9, height = 8)
+plot.new()
+circle_size = unit(1, "snpc")
+pushViewport(viewport(x = 0, y = 0.5, width = circle_size, height = circle_size,
+                      just = c("left", "center")))
+par(omi = gridOMI(), new = TRUE)
+# ---------------------------------------------------------------------------- #
+circos.par(gap.after = c(2, 2, 2, 15, 2))
+circos.heatmap(datAIC, col = col_funAIC, 
+               bg.border = "black", 
+               bg.lwd = 1, 
+               bg.lty = 1,
+               cell.border = "black", 
+               cell.lty = 1, 
+               cell.lwd = 0.25,
+               rownames.side = "outside",
+               track.height = 0.1,
+               split = split,
+               show.sector.labels = TRUE
+)
+circos.heatmap(datR2, col = col_funR2,
+               bg.border = "black", 
+               bg.lwd = 1, 
+               bg.lty = 1,
+               cell.border = "black", 
+               cell.lty = 1, 
+               cell.lwd = 0.25,
+               track.height = 0.1,
+               split = split
+)
+circos.heatmap(datRMSE, col = col_funRMSE,
+               bg.border = "black", 
+               bg.lwd = 1, 
+               bg.lty = 1,
+               cell.border = "black", 
+               cell.lty = 1, 
+               cell.lwd = 0.25,
+               track.height = 0.1,
+               split = split,
+               dend.side = "inside",
+               dend.callback = function(dend, m, si) {
+                 dendsort(dend)
+               }
+)
+circos.track(track.index = 4, panel.fun = function(x, y) {
+  if(CELL_META$sector.numeric.index == 4) {
+    cn = c("RMSE", "r2", "AIC")
+    n = length(cn)
+    circos.text(rep(CELL_META$cell.xlim[2], n) + convert_x(1, "mm"),
+                1:n - 0.5, cn,
+                cex = 0.5, adj = c(0, 0.5), facing = "inside")
+  }
+}, bg.border = NA)
+text(0,0,"BPDIA")
+#circos.text(0,0, labels = "BPDIA")
+# ---------------------------------------------------------------------------- #
+upViewport()
+
+h = dev.size()[2]
+lgd_list = packLegend(lgd_aic, lgd_r2, lgd_rmse, max_height = unit(0.9*h, "inch"))
+draw(lgd_list, x = circle_size, just = "left")
+circos.clear()
 dev.off()
+# ---------------------------------------------------------------------------- #
